74e3d907946f24259a739a2972c4d8ace39570f3
oren wohl <orenwohlwohl@gmail.com>
2022-06-09 00:37:17 +0300

    asdasd

--- src/comps/cart-icon/Cart_icon_component.js
+++ src/comps/cart-icon/Cart_icon_component.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import { CartContext } from "../../context/cart_context";
-import { CartIconContainer, ItemCount } from "./cart-icon.styles";
+import { CartIconContainer, ItemCount } from "./cart-icon.style";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
